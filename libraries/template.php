ef909e3cb7fe74d337aa8a1af565a9e4a076847a
nurmanhabib <nurmanhabib@yahoo.co.id>
2013-09-11 07:09:22 +0700

    menambahkan fungsi add_modal()
    membutuhkan jquery dan bootstrap

--- libraries/template.php
+++ libraries/template.php
@@ -15,12 +15,23 @@ class Template
 	var $css_load = '';
 	var $js_raw = '';
 	var $js_load = '';
+	var $ico_load = '';
 	var $http_headers = array();
 	var $messages = array(
 		'warning' => array(), 
 		'error' => array(), 
 		'success' => array(), 
 		'info' => array()
+	);
+	var $modal = array(
+		'title' => '', 
+		'body' => '', 
+		'button' => array(
+			'type'	=> 'button',
+			'class'	=> 'btn btn-default',
+			'data'	=> 'data-dismiss = "modal"',
+			'value'	=> 'Close'
+			)
 	);
 	
 	
@@ -118,8 +129,31 @@ class Template
 	 * in the data array to be later sent to the template
 	 */
 	function set_content($view, $data = array()){
+
+		// Menyiapkan pesan dan modal agar dapat di akses di view content
+		$this->prepare_messages();
+		$this->prepare_modal();
+		
+		// Menggabungkan data yang di panggil dari controller ke view content
+		$data	= array_merge($data, $this->data);
 		
 		$this->data['content'] = $this->CI->load->view($view, $data, true);
+        
+	}
+
+	
+	
+	/**
+	 * Custom: load konten ke main area yang berada di dalam template tertentu
+	 * Fungsi ini sama halnya dengan set_contet(), hanya saja peletakkan file berada dalam 1 folder template
+	 */
+	function set_content_template($view, $data = array(), $template = ''){
+		
+		// Mengarahkan file content berada dalam template tertentu
+		$view = 'templates/' . ($template == '' ? $this->data['template'] : $template) . '/' . $view;
+		
+		// Memanggil kembali fungsi set_content()
+		$this->set_content($view, $data);
         
 	}
 	
@@ -173,9 +207,9 @@ class Template
 	
 	
 	/**
-	 * Add CSS
+	 * Add JS
 	 * 
-	 * By default, the CSS will be loaded using the normal <link> method
+	 * By default, the JS will be loaded using the normal <script> method
 	 * Optionally, you can choose to have the contents of the file dumped 
 	 * straight to screen to reduce the number of resources the browser
 	 * needs to load at run time
@@ -200,6 +234,30 @@ class Template
 	
 	
 	/**
+	 * Clears all ICO.
+	 */
+	function clear_ico(){
+		
+		$this->data['ico'] = '';
+		
+	}
+	
+	
+	/**
+	 * Add ICO
+	 * Menambahkan favicon untuk browser;
+	 * 
+	 */
+	function add_ico($ico){
+		
+		$this->ico_load .= '<link rel="shortcut icon" href="'.$this->CI->config->item('base_url') . $this->data['assets_dir'] . 'ico/' . $ico . '?'
+			.filemtime($this->data['assets_dir'] . 'ico/' . $ico)
+			.'">';
+		
+	}
+	
+	
+	/**
 	 * Clear all data in the head
 	 */
 	function clear_head(){
@@ -215,6 +273,26 @@ class Template
 	function add_head($head){
 		
 		$this->data['head'] .= $head;
+		
+	}
+	
+	
+	/**
+	 * Clear all data in the foot
+	 */
+	function clear_foot(){
+		
+		$this->data['foot'] = '';
+		
+	}
+	
+	
+	/**
+	 * Add tag to foot
+	 */
+	function add_foot($foot){
+		
+		$this->data['foot'] .= $foot;
 		
 	}
 
@@ -259,6 +337,19 @@ class Template
 	
 	
 	/**
+	 * Menambah sebuah pesan modal
+	 * dengan jquery dan bootstrap
+	 */
+	function add_modal($body, $title = 'Pesan Dialog', $button = array(array())){
+	
+		$this->modal['title'] = $title;
+		$this->modal['body'] = $body;		
+		$this->modal['button'] = $button;
+	
+	}
+	
+	
+	/**
 	 * Serves purely as a wrapper for the CI flashdata
 	 * Just to keep syntax organised
 	 */
@@ -286,17 +377,63 @@ class Template
 			
 			// if there's messages of this type, prepare for printing
 			if(sizeof($messages)){
-				$this->data['messages'] .= '<div class="alert-message '.$type.'">';
-			
 				foreach($messages as $message){
-					$this->data['messages'] .= '<p>'.$message.'</p>';
+					$this->data['messages'] .= '<div class="alert alert-'.$type.'">';
+					$this->data['messages'] .= '<button type="button" class="close" data-dismiss="alert">&times;</button>';
+					$this->data['messages'] .= $message;
+					$this->data['messages'] .= '</div>';
 				}
-			
-				$this->data['messages'] .= '</div>';
 			}
 			
 		}
 	
+	}
+	
+	
+	/**
+	 * Format data dari modal (bootstrap),
+	 * menampilkan pesan dalam bentuk modal
+	 * membutuhkan jquery dan bootstrap
+	 */
+	function prepare_modal(){
+		
+		$modal	= $this->modal;
+
+		// if there's modal of this type, prepare for printing
+		if(count($modal)){
+			// Modal
+			$this->data['messages'] .= '<div class="modal fade" id="rooModal" tabindex="-1" role="dialog" aria-labelledby="rooModalLabel" aria-hidden="true">';
+			$this->data['messages'] .= '<div class="modal-dialog">';
+			$this->data['messages'] .= '<div class="modal-content">';
+
+			// Header
+			$this->data['messages'] .= '<div class="modal-header">';
+			$this->data['messages'] .= '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>';
+			$this->data['messages'] .= '<h4 class="modal-title">'.$modal['title'].'</h4>';
+			$this->data['messages'] .= '</div>';
+
+			// Body
+			$this->data['messages'] .= '<div class="modal-body">'.$modal['body'].'</div>';
+
+			// Footer
+			if(count($modal['button'])){
+				$this->data['messages'] .= '<div class="modal-footer">';
+				foreach ($modal['button'] as $button) {
+					$this->data['messages'] .= '<button type="'.$button['type'].'" ';
+					$this->data['messages'] .= 'class="'.$button['class'].'" ';
+					$this->data['messages'] .= $button['data'];
+					$this->data['messages'] .= '>'.$button['value'].'</button>';
+				}
+				$this->data['messages'] .= '</div>';
+			}
+
+			// Close tag
+			$this->data['messages'] .= '</div>'; //.modal-content
+			$this->data['messages'] .= '</div>'; //.modal-dialog
+			$this->data['messages'] .= '</div>'; //.modal
+
+			$this->add_foot('<script type="text/javascript">$("#rooModal").modal()</script>');
+		}
 	}
 	
 	
@@ -328,12 +465,64 @@ class Template
 	
 	
 	/**
+	 * Menambahkan tag ico
+	 * ico file
+	 */
+	function prepare_ico(){
+
+		$this->data['ico']	.= $this->ico_load;
+			
+	}
+		
+	
+	/**
+	 * Custom: Mengambil value dari item konfigurasi
+	 */
+	function config($item){
+		return $this->data[$item];
+	}
+	
+	
+	
+	/**
+	 * Custom: Mengganti konfigurasi
+	 */
+	function set_config($template_conf = array()){
+		foreach($template_conf as $citem => $cval)
+		{
+			$this->data[$citem] = $cval;
+		}
+		unset($template_conf);
+	}
+	
+	
+	
+	/**
+	 * Custom: Mengganti template
+	 */
+	function set_template($template = 'default'){	
+		$this->set_config(array('template' => $template));		
+	}
+	
+	
+	/**
+	 * Custom: Mengambil pesan
+	 */
+	function messages(){
+		$this->prepare_messages();
+		return $this->config('messages');
+	}
+	
+	
+	/**
 	 * Send the data compiled data to the screen
 	 */
 	function build(){
 	
 		$this->prepare_jcss();
+		$this->prepare_ico();
 		$this->prepare_messages();
+		$this->prepare_modal();
 		$this->build_http_header();
 		
 		$this->CI->load->view('templates/'.$this->data['template'].'/index.php', $this->data);
